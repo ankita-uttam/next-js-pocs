1148d7b11ced41a73e1b2e62cbe7d21835c24a88
Ankita.uttam <ankita.uttam@IN-Ankitauttam.local>
2019-08-19 12:47:42 +0530

    Add. Navigation in home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 const index = () => (
     <div>
+        {/* This is the comment expression in JSX */}
+        {/* creates a navigation link in home page that navigates to about page */}
+        <Link href="/about">
+            <a> About </a>
+        </Link>
         <p>Hello Next.js</p>
     </div>
 );
